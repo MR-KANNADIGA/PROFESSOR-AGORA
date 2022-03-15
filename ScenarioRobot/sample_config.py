459455aeafa05de3cdde0cf584aadb67c2c632b9
ᴹᴿ KANNADIGA <97476963+MR-KANNADIGA@users.noreply.github.com>
2022-03-15 10:25:42 +0530

    Update sample_config.py

--- ScenarioRobot/sample_config.py
+++ ScenarioRobot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/ScenarioRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/PROFESSOR-AGORA/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 2144966937  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "userderdead"
-    SUPPORT_CHAT = "OfflineWalker"  # Your own group for support, do not add the @
+    OWNER_ID = 5201305448  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Mr_StonedLegend"
+    SUPPORT_CHAT = "Masti_in_dosti"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        --1001570490489
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        --1001570490489
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
@@ -35,7 +35,7 @@ class Config(object):
     INFOPIC = True
     URL = None
     SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
-    SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
+    SPAMWATCH_SUPPORT_CHAT = "@dangerouskannadiga"
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
